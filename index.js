b945860156e84efca4e3d09fac87d9f1a775d681
Linda Gift <lindagift0812@gmail.com>
2024-08-29 17:01:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,48 +10,48 @@ let reumeSection = document.getElementById("resume")
 let portFolio = document.getElementById("portfolio")
 let testimonials = document.getElementById("testimonials")
 let contacts = document.getElementById("contacts")
-// let aboutMe = document.getElementById("home2")
+let knowMoreButton = document.getElementById("know-more")
+let contactMeButton = document.getElementById("contactme")
 
 aboutMe.addEventListener("keyup", function(event){
     if (event.key === "Enter") { // Check if the Enter key is pressed  
         window.location.hash = "aboutMeSection" 
     }
-    console.log(aboutMe)
-    
   })
 
 resume.addEventListener("keyup", function(event){
-    if (event.key === "Enter") { // Check if the Enter key is pressed  
+    if (event.key === "Enter") { 
         window.location.hash = "resumeSection" 
     }
-    console.log(aboutMe)
-    
   })
 
 portFolio.addEventListener("keyup", function(event){
     if (event.key === "Enter") { // Check if the Enter key is pressed  
         window.location.hash = "portfolio" 
     }
-    console.log(aboutMe)
-    
   })
 
 testimonial.addEventListener("keyup", function(event){
     if (event.key === "Enter") { // Check if the Enter key is pressed  
         window.location.hash = "testimonials" 
     }
-    console.log(aboutMe)
-    
   })
 
 contacts.addEventListener("keyup", function(event){
     if (event.key === "Enter") { // Check if the Enter key is pressed  
         window.location.hash = "contacts" 
-    }
-    console.log(aboutMe)
-    
+    }    
   })
 
 cvButton.addEventListener("click", function(){
     window.location.href = "https://www.figma.com/design/LySKx0zxE2EbTLJ8ZO1AEU/Linda-Resume-(Copy)?node-id=0-1&node-type=CANVAS&t=BSr9sXenvATQH10F-0"
 })
+
+contactMeButton.addEventListener("click", function(){
+  window.location.hash = "contacts"
+})
+
+knowMoreButton.addEventListener("click", function(){
+  window.location.hash = "aboutMeSection"
+})
+    
